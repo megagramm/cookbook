66f36b78ec19fb9a61573f3a53196ae60958518e
Andrey Grey <megagramm@gmail.com>
2022-11-05 09:06:14 +0300

    Убраны лишние принты

--- main.py
+++ main.py
@@ -11,30 +11,29 @@ with open('recipes.txt') as file:
             ingredients_tmp = file.readline().strip().split(' | ')
             product, product_count, product_measure = ingredients_tmp
             ingredients.append({'ingredient_name': product,
-                              'quantity': int(product_count),
-                              'measure': product_measure})
+                                'quantity': int(product_count),
+                                'measure': product_measure})
         file.readline()
         dishes.append({dishe_name: ingredients})
 
-# print(*dishes, sep="\n")
-
+print(*dishes, sep="\n")
+print()
 # exit()
 
 
-def get_shop_list_by_dishes(dishes_list: list, person_count: int):
+def get_shop_list_by_dishes(list_of_dishes: list, person_count: int):
     shoping_list = dict()
-    # shoping_list.update({"Картофель": {'measure': 'кг', 'quantity': 2}})
     for dishe_dict in dishes:
         for dishe in dishe_dict.keys():
-            if dishe in dishes_list:
-                print(dishe_dict.values())
+            if dishe in list_of_dishes:
+                # print(dishe_dict.values())
                 for ingredient in dishe_dict.get(dishe):
-                    print(ingredient)
+                    # print(ingredient)
                     if shoping_list.get(ingredient['ingredient_name']):
-                        print(ingredient['ingredient_name'], 'уже в словаре')
+                        # print(ingredient['ingredient_name'], 'уже в словаре')
                         shoping_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity']*person_count
                     else:
-                        shoping_list.update({ingredient['ingredient_name']:{ingredient['ingredient_name']: ingredient['measure'], 'quantity': ingredient['quantity']*person_count}})
+                        shoping_list.update({ingredient['ingredient_name']: {ingredient['ingredient_name']: ingredient['measure'], 'quantity': ingredient['quantity']*person_count}})
     print(shoping_list)
 
 
