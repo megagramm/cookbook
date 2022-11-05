782f617790f202600904fab36dc72b470596c963
Andrey Grey <megagramm@gmail.com>
2022-11-05 13:28:04 +0300

    Переделан проект на функции

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-with open('recipes.txt') as file:
-    dishes = []
-    for line in file:
-        first_line = line.strip()
-        while first_line == "" or first_line == "\n":
-            first_line = file.readline().strip()
-        dishe_name = first_line
-        dishe_count = int(file.readline())
-        ingredients = []
-        for _ in range(dishe_count):
-            ingredients_tmp = file.readline().strip().split(' | ')
-            product, product_count, product_measure = ingredients_tmp
-            ingredients.append({'ingredient_name': product,
-                                'quantity': int(product_count),
-                                'measure': product_measure})
-        file.readline()
-        dishes.append({dishe_name: ingredients})
+def make_cookbook():
+    with open('recipes.txt') as file:
+        cookbook = []
+        for line in file:
+            first_line = line.strip()
+            while first_line == "" or first_line == "\n":
+                first_line = file.readline().strip()
+            dishe_name = first_line
+            dishe_count = int(file.readline())
+            ingredients = []
+            for _ in range(dishe_count):
+                ingredients_tmp = file.readline().strip().split(' | ')
+                product, product_count, product_measure = ingredients_tmp
+                ingredients.append({'ingredient_name': product,
+                                    'quantity': int(product_count),
+                                    'measure': product_measure})
+            file.readline()
+            cookbook.append({dishe_name: ingredients})
+    return cookbook
 
-print(*dishes, sep="\n")
+
+print(*make_cookbook(), sep="\n")
 print()
-# exit()
 
 
 def get_shop_list_by_dishes(list_of_dishes: list, person_count: int):
+    dishes = make_cookbook()
     shoping_list = dict()
     for dishe_dict in dishes:
         for dishe in dishe_dict.keys():
